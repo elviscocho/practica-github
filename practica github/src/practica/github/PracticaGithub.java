75575972aa2fa39cf89a772add25c24b18a86e92
admin <admin@Win7_1TB>
2018-11-05 09:34:49 +0100

    inicial

---
+++ practica github/src/practica/github/PracticaGithub.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica.github;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author admin
+ */
+public class PracticaGithub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    
+     String cadena;
+        boolean seguir;
+        int con=0;
+          Scanner sc=new Scanner(System.in);
+   
+        do
+        {
+            System.out.println("anota un caracter");
+            char caracter=sc.nextLine().charAt(0);
+            caracter=Character.toUpperCase(caracter);
+            System.out.println("anota una cadena");
+            cadena=sc.nextLine();
+            cadena=cadena.toUpperCase();
+            con=0;
+            for (int i=0;i<cadena.length();i++)
+                 if (cadena.charAt(i)==caracter)
+                     con++;
+            System.out.println("se han anotado: "+con+" veces el caracter: "+caracter);
+            System.out.println("¿deseas escribir otra?");
+            seguir=sc.nextBoolean();
+            sc.nextLine();
+        }
+        while (seguir==true);
+    }
+    
+}
